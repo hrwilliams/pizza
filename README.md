c61162dd97fecfc7d09ccbcdf97c2b951789931a
Halle Williams <williams.hr@gmail.com>
2016-06-17 08:37:01 -0700

    add initial file structure, start README with specifications

---
+++ README.md
@@ -0,0 +1,60 @@
+# address-book
+
+##### Epicodus Section: JavaScript OO - Address Book
+
+#### By HR Williams
+
+## Description
+
+This application is a demonstration of constructors and prototypes using JavaScript to create a simple website where a user can order a pizza and find out the price.
+
+
+## Specifictions
+Behavior  | Input Example | Output Example
+------------- | ------------- | -------------
+A user will choose pizza size and a base price will be added | small | $12 will be added to price
+A user will choose toppings and $2 will be added for each topping | mushrooms | $2 will be added to the price.
+
+## Setup
+
+Install address-book by cloning this repository.
+
+
+## Technologies Used
+
+JavaScript and jQuery
+
+### Legal
+
+Copyright (c) 2016 **HR Williams**
+
+This software is licensed under the MIT license.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.lowing conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ js/scripts.js
@@ -0,0 +1,108 @@
+// // //busines logic
+// function Ticket(run, time, age) {
+//   // this.movie = movie;
+//   this.run = run;
+//   this.time = time;
+//   this.age = age;
+//   this.price="";
+// }
+//
+// Ticket.prototype.ticketPrice = function(){
+//   //i think that ticketprice should calculate a price and then set this's price to the result
+//   //of those calculations and then return it.
+//   return this.run - this.time - this.age;
+// }
+//
+// //user interface logic
+// $(document).ready(function() {
+//   $("form#movies").submit(function(event){
+//       event.preventDefault();
+//     // var run = parseInt($("#run").val());//change this to movie title
+//     var run = parseInt($("#run").val());
+//     var time = parseInt($("#time").val());
+//     var age = parseInt($("#age").val());
+//     var newTicket = new Ticket(run, time, age);
+//     $("#show-price").show();
+//     $("#price").text(newTicket.ticketPrice());
+//   });
+//
+// });
+
+
+
+//business logic
+function Pizza(customer) {
+  this.customer = customer;
+  this.price = [];
+}
+
+function Price(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state;
+}
+
+// user interface logic
+$(document).ready(function() {
+
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Street</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City</label>' +
+                                   '<input type="text" class="form-control new-city">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-state">State</label>' +
+                                   '<input type="text" class="form-control new-state">' +
+                                 '</div>' +
+                               '</div>');
+  });
+
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find("input.new-street").val();
+      var inputtedCity = $(this).find("input.new-city").val();
+      var inputtedState = $(this).find("input.new-state").val();
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState)
+      newContact.addresses.push(newAddress)
+    });
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.fullName());
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+      $("ul#addresses").text("");
+      newContact.addresses.forEach(function(address) {
+        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+      });
+    });
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-state").val("");
+
+  });
+});
