4736e57311e46bf17278767a8959a6de13e435d2
Halle Williams <williams.hr@gmail.com>
2016-06-17 16:29:41 -0700

    save changes for end of day, add more checkboxes for toppings, hide toppings before size is chosen

--- js/scripts.js
+++ js/scripts.js
@@ -37,26 +37,42 @@ function Pizza(customer) {
 // Pizza.prototype.order = function() {
 //   return this.customer + " " + this.price;
 
-function Price(size, mushroom, feta) {
+function Price(size, goatCheese, spinach, roastedPotatoes, redOnion, pinapple, basil, pear, mushrooms, sunDriedTomatoes) {
   this.size = size;
-  this.mushroom = mushroom;
-  this.feta = feta;
+  this.goatCheese = goatCheese;
+  this.spinach = spinach;
+  this.roastedPotatoes = roastedPotatoes;
+  this.redOnion = redOnion;
+  this.pinapple = pinapple;
+  this.basil = basil;
+  this.pear = pear;
+  this.mushrooms = mushrooms;
+  this.sunDriedTomatoes = sunDriedTomatoes;
 }
 
 Price.prototype.totalPrice = function() {
-  return this.size + this.mushroom + this.feta;
+  return this.size + this.goatCheese + this.spinach + this.roastedPotatoes + this.redOnion + this.pinapple + this.basil + this.pear + this.mushrooms + this.sunDriedTomatoes;
 }
 
 // / //user interface logic
 $(document).ready(function() {
-  $("form#pizza").submit(function(event){
+  $("form#Pizza").submit(function(event){
       event.preventDefault();
     // var customer = $("#customer").val();
     var size = parseInt($("#size").val());
     // var  = parseInt($("#topping").val());
-    var mushroom = parseInt($("#mushroom").val());
-    var feta = parseInt($("#feta").val());
-    var newPrice = new Price(size, mushroom, feta);
+    $("#show-toppings").show();
+
+    var goatCheese = parseInt($("#goat-cheese").val());
+    var spinach = parseInt($("#spinach").val());
+    var roastedPotatoes = parseInt($("#roasted-potatoes").val());
+    var redOnion = parseInt($("#red-onion").val());
+    var pinapple = parseInt($("#pinapple").val());
+    var basil = parseInt($("#basil").val());
+    var pear = parseInt($("#pear").val());
+    var mushrooms = parseInt($("#mushrooms").val());
+    var sunDriedTomatoes = parseInt($("#sun-dried-tomatoes").val());
+    var newPrice = new Price(size, goatCheese, spinach, roastedPotatoes, redOnion, pinapple, basil, pear, mushrooms, sunDriedTomatoes);
     // var newPizza = new Pizza(customer)
     $("#show-price").show();
     $("#price").text(newPrice.totalPrice());
